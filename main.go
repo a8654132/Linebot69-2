61be453ea52ab41c5975f03679f7b5983d337b87
a8654132 <31846955+a8654132@users.noreply.github.com>
2017-10-11 02:56:34 +0800

    QQ
    QQ

--- main.go
+++ main.go
@@ -1,15 +1,3 @@
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package main
 
 import (
@@ -17,7 +5,9 @@ import (
 	"log"
 	"net/http"
 	"os"
-
+	// "context"
+	//"time"
+	"encoding/json"
 	"github.com/line/line-bot-sdk-go/linebot"
 )
 
@@ -27,7 +17,37 @@ func main() {
 	var err error
 	bot, err = linebot.New(os.Getenv("ChannelSecret"), os.Getenv("ChannelAccessToken"))
 	log.Println("Bot:", bot, " err:", err)
+
+
 	http.HandleFunc("/callback", callbackHandler)
+
+
+	// fmt.Println(prof)
+	binary, _ := Redis_Get(mac)
+	user := new(USER_MAC)
+	json.Unmarshal(binary,&user)
+	onlyonecontent := user.USER[1].CONTENT
+	// var allcontent string
+	 // for i:=0;i < len(user.USER) ; i++{
+	 //  allcontent = allcontent+user.USER[i].CONTENT
+	 // }
+
+	template := linebot.NewButtonsTemplate(
+			"", "請告訴我們，您對於以上文章的評價" , "" ,
+			linebot.NewPostbackTemplateAction("滿意 :)", "Y" , ""),
+			linebot.NewPostbackTemplateAction("不滿意 :(", "N" , ""),
+	)
+
+	bot.PushMessage(
+		"Uecc089487f1487a78637be4e2fe3dca9",
+		linebot.NewTextMessage(onlyonecontent)).Do()
+
+	bot.PushMessage(
+		"Uecc089487f1487a78637be4e2fe3dca9",
+		linebot.NewTemplateMessage("今日文章", template)).Do()
+
+
+
 	port := os.Getenv("PORT")
 	addr := fmt.Sprintf(":%s", port)
 	http.ListenAndServe(addr, nil)
@@ -44,14 +64,30 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 		}
 		return
 	}
-
+	// ctx, cancel := context.WithTimeout(context.Background(), 5*time.Millisecond)
+	// defer cancel()
 	for _, event := range events {
-		if event.Type == linebot.EventTypeMessage {
-			switch message := event.Message.(type) {
-			case *linebot.TextMessage:
-				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.ID+":"+message.Text+" OK!")).Do(); err != nil {
+		if event.Type == linebot.EventTypeFollow {
+			prof := event.Source.UserID
+			follow := linebot.NewButtonsTemplate(
+					"", "歡迎您使用本服務!", "你好，我是中央大學的曾怡雯，\n請按以下按鈕做出對應的動作：",
+					linebot.NewPostbackTemplateAction("我要輸入我的MAC", "AddMAC",""),
+					linebot.NewPostbackTemplateAction("我要更正我的MAC", "ModifyMAC",""),
+					linebot.NewURITemplateAction("我想看怡雯畫的阿卡莉", "https://www.pixiv.net/member_illust.php?mode=medium&illust_id=62861397"),
+			)
+
+			if _, err := bot.PushMessage(prof, linebot.NewTemplateMessage("Smart AP <3", follow)).Do(); err != nil {
 					log.Print(err)
-				}
+			}
+		}
+		if event.Type == linebot.EventTypePostback {
+			prof := event.Source.UserID
+			data := event.Postback.Data
+			if data == "AddMAC"{
+				bot.PushMessage(prof, linebot.NewTextMessage("現在請輸入你的MAC：")).Do()
+			}
+			if data == "ModifyMAC"{
+				bot.PushMessage(prof, linebot.NewTextMessage("現在請輸入你要更正的MAC：")).Do()
 			}
 		}
 	}
